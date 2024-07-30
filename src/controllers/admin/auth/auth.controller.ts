d58026a758cc8ab490f15f2e6ec24e7cfbd98625
Jattin Manhas <jattinmanhas0102@gmail.com>
2024-07-31 02:16:44 +0530

    Completed Login Feature for Admin Role

--- src/controllers/admin/auth/auth.controller.ts
+++ src/controllers/admin/auth/auth.controller.ts
@@ -1,11 +1,57 @@
 import { NextFunction, Request, Response } from "express";
 import { asyncHander } from "../../../utils/handlers/asyncHander";
-import { createUser, emailExists, usernameExists } from "../../../services/auth.services";
+import {
+  createUser,
+  createUserAuthSettings,
+  emailExists,
+  isEmail,
+  loginServiceforAdmin,
+  usernameExists,
+} from "../../../services/auth.services";
 import { Role } from "@prisma/client";
 
 export const login = asyncHander(async (req: Request, res: Response) => {
-    console.log("inside login");
-})
+  const username = req.body.username;
+  const password = req.body.password;
+  let mail = false;
+
+  if (isEmail(username)) {
+    mail = true;
+    const emailExist: boolean = await emailExists(username);
+    if (!emailExist) {
+      return res.status(404).json({ message: "User not found ..." });
+    }
+  } else {
+    const usernameExist: boolean = await usernameExists(username);
+    if (!usernameExist) {
+      return res.status(400).json({ message: "User Not found..." });
+    }
+  }
+
+  const user = await loginServiceforAdmin(username, password, Role.ADMIN, mail);
+  if (user.flag) {
+    return res.status(400).json({ message: user.message });
+  }
+
+  // set tokens to the cookies
+  const token = user.tokens?.token;
+  const refresnToken = user.tokens?.refreshToken;
+
+  // Set the JWT as an HTTP-only cookie
+  res.cookie("token", token, {
+    httpOnly: true, // prevents JavaScript access
+    secure: false, // set to true if using HTTPS
+    maxAge: 15 * 60 * 1000, // 1 hour in milliseconds
+  });
+
+  res.cookie("refresh_token", refresnToken, {
+    httpOnly: true,
+    secure: false, // set to true if using HTTPS
+    maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+  });
+
+  return res.status(200).json({message: "Successfully Logged In..."})
+});
 
 /**
  * @description : user registration
@@ -14,27 +60,39 @@ export const login = asyncHander(async (req: Request, res: Response) => {
  * @return {Object} : response for register {status, message, data}
  */
 
-export const register = asyncHander(async(req: Request, res: Response) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const password = req.body.password;
-    const fullname = req.body.fullname;
-    const mobileNo = req.body.mobileNo;
+export const register = asyncHander(async (req: Request, res: Response) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const fullname = req.body.fullname;
 
-    const usernameExist: boolean = await usernameExists(username);
-    if(usernameExist){
-        return res.status(400).json({message: "Username Already Exists..."});
-    }
-    const emailExist: boolean = await emailExists(email);
-    if(emailExist){
-        return res.status(400).json({message: "Email already exists..."});
-    }
+  const usernameExist: boolean = await usernameExists(username);
+  if (usernameExist) {
+    return res.status(400).json({ message: "Username Already Exists..." });
+  }
+  const emailExist: boolean = await emailExists(email);
+  if (emailExist) {
+    return res.status(400).json({ message: "Email already exists..." });
+  }
 
-    const user = await createUser(username, email, password, fullname, Role.ADMIN);
+  const user = await createUser(
+    username,
+    email,
+    password,
+    fullname,
+    Role.ADMIN
+  );
+  if (user.flag) {
+    return res.status(400).json({ message: user.message });
+  }
 
-    if(!user){
-        return res.status(400).json({message: "Failed to create User..."});
-    }
+  const userAuth = await createUserAuthSettings(user.data?.id!);
 
-    return res.status(200).json({message: "User created successfully..."});
-})
+  if (userAuth.flag) {
+    return res.status(400).json({ message: userAuth.message });
+  }
+
+  return res
+    .status(200)
+    .json({ data: user.data, message: "User created successfully..." });
+});
--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,11 +1,27 @@
-import { PrismaClient, Role } from "@prisma/client";
+import { PrismaClient, Role, Prisma } from "@prisma/client";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
-interface returnResponse {
+interface UserPayload {
+  id: string;
+  username: string;
+  email: string;
+  role?: Role;
+  password?: string;
+}
+
+interface Tokens {
+  token: string;
+  refreshToken: string;
+}
+
+interface ReturnPayload {
   flag: boolean;
-  message: string;
+  data?: UserPayload;
+  tokens?: Tokens;
+  message: String;
 }
 
 /**
@@ -43,7 +59,7 @@ export const emailExists = async (mail: string): Promise<boolean> => {
 /**
  * @description : Create new User
  * @param {string} username : username entered by user
- * @return {object} : Return true if user exists else false
+ * @return {object} : Return userdata, flag and message
  */
 
 export const createUser = async (
@@ -52,11 +68,11 @@ export const createUser = async (
   password: string,
   fullname?: string,
   role: Role = Role.USER
-): Promise<boolean> => {
+): Promise<ReturnPayload> => {
   try {
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: {
         username: username,
         email: email,
@@ -64,11 +80,294 @@ export const createUser = async (
         fullname: fullname,
         role: role,
       },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        role: true,
+      },
     });
 
-    return true;
+    return {
+      flag: false,
+      data: user,
+      message: "User Created Successfully...",
+    };
   } catch (error) {
     console.log(error);
-    return false;
+    return {
+      flag: true,
+      message: "Failed to Create New User...",
+    };
   }
+};
+
+/**
+ * @description : Create new User auth Settings Entry
+ * @param {string} userid : username entered by user
+ * @return {object} : Return Return Payload
+ */
+
+export const createUserAuthSettings = async (
+  userId: string
+): Promise<ReturnPayload> => {
+  try {
+    await prisma.userAuthSettings.create({
+      data: {
+        userId: userId,
+      },
+    });
+
+    return {
+      flag: false,
+      message: "User Auth Settings Created Successfully...",
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      flag: true,
+      message: "Failed to Create User Auth Settings",
+    };
+  }
+};
+
+/**
+ * @description : Checks if the particular String is email or not
+ * @param {string} input : username or email entered by user
+ * @return {object} : Return true if entered string is email else false
+ */
+
+export function isEmail(input: string): boolean {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(input);
+}
+
+function constructWhereClause(
+  field: "username" | "email",
+  value: string
+): Prisma.userWhereUniqueInput {
+  switch (field) {
+    case "username":
+      return { username: value };
+    case "email":
+      return { email: value };
+    default:
+      throw new Error(`Invalid field: ${field}`);
+  }
+}
+
+export const getUser = async (
+  username: string,
+  includePassword: boolean,
+  field: "username" | "email"
+): Promise<UserPayload> => {
+  try {
+    const whereClause = await constructWhereClause(field, username);
+
+    const user = await prisma.user.findUnique({
+      where: whereClause,
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        role: true,
+        password: includePassword,
+      },
+    });
+
+    return user!;
+  } catch (error) {
+    console.error(`Error finding user by ${field}:`, error);
+    throw error;
+  }
+};
+
+export const getUserAuthSettings = async (userId: string) => {
+  try {
+    const userAuth = await prisma.userAuthSettings.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return userAuth;
+  } catch (error) {
+    console.error(`Error finding user Auth Settings:`, error);
+    throw error;
+  }
+};
+
+export const checkPasswordCorrect = async (
+  password: string,
+  hashedPassword: string
+): Promise<boolean> => {
+  const match = await bcrypt.compare(password, hashedPassword);
+
+  return match;
+};
+
+function getDifferenceOfTwoDatesInMinutes(date1: Date, date2: Date) {
+  const diffInMilliseconds = date2.getTime() - date1.getTime();
+  const diffInMinutes = diffInMilliseconds / 60000; // Convert milliseconds to minutes
+  return Math.ceil(Math.abs(diffInMinutes)); // Return the absolute value to ensure the difference is positive
+}
+
+export const loginServiceforAdmin = async (
+  username: string,
+  password: string,
+  role: Role,
+  isMail: boolean
+): Promise<ReturnPayload> => {
+  let user;
+
+  if (isMail) {
+    user = await getUser(username, true, "email");
+  } else {
+    user = await getUser(username, true, "username");
+  }
+
+  let userAuth = await getUserAuthSettings(user.id);
+
+  let currentTime = new Date();
+  let expireTime = new Date(currentTime.getTime() + 30 * 60 * 1000);
+
+  if (userAuth && userAuth.loginRetryLimit >= 3) {
+    const limitTime = userAuth.loginReactiveTime
+      ? new Date(userAuth.loginReactiveTime)
+      : null;
+
+    if (limitTime && limitTime > currentTime) {
+      if (limitTime <= expireTime) {
+        return {
+          flag: true,
+          message: `You have exceeded the number of attempts. You can login after ${getDifferenceOfTwoDatesInMinutes(
+            currentTime,
+            limitTime
+          )} minutes.`,
+        };
+      }
+
+      await prisma.userAuthSettings.update({
+        where: { userId: user.id },
+        data: {
+          loginReactiveTime: expireTime,
+          loginRetryLimit: userAuth.loginRetryLimit! + 1,
+        },
+      });
+
+      return {
+        flag: true,
+        message: `You have exceeded the number of attempts. You can login after ${getDifferenceOfTwoDatesInMinutes(
+          currentTime,
+          expireTime
+        )} minutes.`,
+      };
+    } else {
+      await prisma.userAuthSettings.update({
+        where: {
+          userId: user.id,
+        },
+        data: {
+          loginRetryLimit: userAuth?.loginRetryLimit! + 1,
+          loginReactiveTime: expireTime,
+        },
+      });
+
+      return {
+        flag: true,
+        message: `You have exceed the number of limit.you can login after ${getDifferenceOfTwoDatesInMinutes(
+          currentTime,
+          expireTime
+        )} minutes`,
+      };
+    }
+  }
+
+  const isPasswordMatch = await checkPasswordCorrect(password, user.password!);
+  if (!isPasswordMatch) {
+    await prisma.userAuthSettings.update({
+      where: {
+        userId: user.id,
+      },
+      data: {
+        loginRetryLimit: userAuth?.loginRetryLimit! + 1,
+      },
+    });
+
+    return {
+      flag: true,
+      message: "Incorrect Password",
+    };
+  }
+
+  // generating tokens if password is correct...
+  let tokens: Tokens = {
+    token: await generateJwtToken(user),
+    refreshToken: await generateRefreshToken(user),
+  };
+
+  // resetting login retry limit and time...
+  if (userAuth && userAuth.loginRetryLimit > 0) {
+    await prisma.userAuthSettings.update({
+      where: {
+        userId: user.id,
+      },
+      data: {
+        loginRetryLimit: 0,
+        loginReactiveTime: null
+      },
+    });
+  }
+
+  return {
+    flag: false,
+    tokens: tokens,
+    data: await removePassword(user),
+    message: "Log In Success...",
+  };
+
+};
+
+async function removePassword(user: UserPayload){
+  const { password, ...userWithoutPassword } = user;
+
+  return userWithoutPassword;
+}
+
+export const generateJwtToken = async (user: UserPayload) => {
+  const JWT_SECRET =
+    user.role == "ADMIN" ? process.env.ADMIN_SECRET : process.env.CLIENT_SECRET;
+
+  const payload = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+    role: user.role,
+  };
+
+  const token = await sign(payload, JWT_SECRET!, {
+    expiresIn: "15m",
+  });
+
+  return token;
+};
+
+export const generateRefreshToken = async (user: UserPayload) => {
+  const REFRESH_SECRET =
+    user.role == "ADMIN"
+      ? process.env.REFRESH_ADMIN_SECRET
+      : process.env.REFRESH_CLIENT_SECRET;
+
+  const payload = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+    role: user.role,
+  };
+
+  const refreshToken = await sign(payload, REFRESH_SECRET!, {
+    expiresIn: "1d",
+  });
+
+  return refreshToken;
 };
